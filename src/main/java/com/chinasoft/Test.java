b8acf8e379b286dd19447c87fa278a3266b3dad8
wangyule <635249036@qq.com>
2018-01-07 22:45:17 +0800

    1

--- src/main/java/com/chinasoft/Test.java
+++ src/main/java/com/chinasoft/Test.java
@@ -3,7 +3,7 @@ package com.chinasoft;
 public class Test {
 	public static void main(String[] args) {
 	System.out.println("测试哟用的");
-	System.out.println("新添加的分支");
+	System.out.println("新添加的分支 1");
 	System.out.println("再来呢");
 	}
 }
