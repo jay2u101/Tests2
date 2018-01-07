a8b6a135a96378fa3b192658df9213faf82ea049
wangyule <635249036@qq.com>
2018-01-07 23:15:52 +0800

    1111111

--- src/main/java/com/chinasoft/Test.java
+++ src/main/java/com/chinasoft/Test.java
@@ -3,7 +3,7 @@ package com.chinasoft;
 public class Test {
 	public static void main(String[] args) {
 	System.out.println("测试哟用的");
-	System.out.println("再来呢");
-	System.out.println("111");
+	System.out.println("再来呢1");
+	System.out.println("1111");
 	}
 }
