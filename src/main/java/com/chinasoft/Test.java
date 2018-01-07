78aeb52418b917eb47a1b90bbcd864af9926a48c
wangyule <635249036@qq.com>
2018-01-07 23:23:32 +0800

    333

--- src/main/java/com/chinasoft/Test.java
+++ src/main/java/com/chinasoft/Test.java
@@ -3,7 +3,9 @@ package com.chinasoft;
 public class Test {
 	public static void main(String[] args) {
 	System.out.println("测试哟用的");
+	System.out.println("再来呢1");
 	System.out.println("再来呢11");
 	System.out.println("111");
+	System.out.println("1111");
 	}
 }
